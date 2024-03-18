20d29735da76e48e0df9ad95f77e9937aecdf66e
pooja123-yadav <85959414+pooja123-yadav@users.noreply.github.com>
2024-03-18 12:27:18 +0530

    Update Linked_list.py

--- Linked_list.py
+++ Linked_list.py
@@ -150,7 +150,31 @@ class Solution(object):
         current.next = current.next.next
 
         return head    
+# ============================================================ optimized solution with slo and fast pointer
 
+# Create two pointers, fastp and slowp
+        fastp = head
+        slowp = head
+
+        # Move the fastp pointer N nodes ahead
+        for i in range(n):
+            fastp = fastp.next
+
+        # If fastp becomes None, the Nth node from the end is the head
+        if fastp is None:
+            return head.next
+
+        # Move both pointers until fastp reaches the end
+        while fastp.next is not None:
+            slowp = slowp.next
+            fastp = fastp.next
+
+        # Delete the Nth node from the end
+        delNode = slowp.next
+        slowp.next = slowp.next.next
+        delNode = None
+
+        return head
 
 
         
